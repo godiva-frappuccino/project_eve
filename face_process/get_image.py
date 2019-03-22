f31a35a1250ecc0939bd309a2ba9301c66e95d03
godiva-frappuccino <miki.10pro@gmail.com>
2019-03-23 00:20:53 +0900

    move files

---
+++ face_process/get_image.py
@@ -0,0 +1,9 @@
+import cv2
+import picamera
+
+class Camera_module:
+    def __init__(self):
+        self.godiva = True
+    def get_image(self):
+        image = None
+        return image
--- face_process/main_process.py
+++ face_process/main_process.py
@@ -1,1 +1,22 @@
+import cv2
+import numpy as np
+from get_image import CameraModule
+from face_detector import FaceDetector
+import time
+from motor import Motor
 
+def main():
+    # init
+    camera = CameraModule()
+    detector = FaceDetector()
+    motor = Motor()
+    while True:
+        # detect human        
+        image = camera.get_image()
+        position_diff = detector.get_position()
+        # sleep
+        time.sleep(0.5)
+        # face move
+        motor.adjust(position_diff)
+        # sleep
+        time.sleep(0.5)
